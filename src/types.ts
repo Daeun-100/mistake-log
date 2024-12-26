6f68b21c923a2fc95039f74f13b11dfbf8004ed2
Ko Daeun <drechsu@gmail.com>
2024-12-26 16:06:55 +0900

    refact(tayps) : 유틸리티 타입 Pick을 이용해 타입 재정의

--- src/types.ts
+++ src/types.ts
@@ -1,18 +1,27 @@
-export type TextFormFields = {
-  description: string;
-  solution: string;
-  insights: string;
-};
-export type LogItemFields = {
+//FormField에서 description:, solution:, insights: 만 뽑아서 저장
+//pick 함수를 사용해서 뽑아내기
+
+export type FormFields = {
   timestamp: string;
   title: string;
   category: string[];
   favorite: boolean;
-};
-type otherFields = {
+  description: string;
+  solution: string;
+  insights: string;
   severity: number;
   frequency: number;
   relatedMistakes?: number[];
-  status?: string; //  "archived"
+  status: 'archived' | 'active';
 };
-export type FormFields = TextFormFields & LogItemFields & otherFields;
+
+export type TextFormFields = Pick<
+  FormFields,
+  'description' | 'solution' | 'insights'
+>;
+
+//LogItemFields도 위와 같이 Pick 함수를 사용해서 뽑아내기
+export type LogItemFields = Pick<
+  FormFields,
+  'timestamp' | 'title' | 'category' | 'favorite'
+>;
