b98f021290c2942d56174fe92cfc1762e772bb2a
Ko Daeun <drechsu@gmail.com>
2024-12-26 20:41:30 +0900

    feat(atom) : atom 파일 생성 및 로그 리스트 전역상태로 변경

---
+++ src/atom.ts
@@ -0,0 +1,43 @@
+import { atom } from 'jotai';
+import { FormFields } from './types';
+
+const mockDatalist: FormFields[] = [
+  {
+    timestamp: '2021-09-01',
+    title: 'title1sdf',
+    category: ['category1', 'category2'],
+    favorite: true,
+    description: 'description1lsdkfjlsdkfjlsdkfjalsdkfjalsdfkjaldfkjaldkfjl',
+    solution: 'solution1sdf.kjsdlfkjsdlfkjsldfkja',
+    insights: 'insights1dfglkjfdlgkdjfgl',
+    severity: 1,
+    frequency: 1,
+    status: 'active',
+  },
+  {
+    timestamp: '2021-09-02',
+    title: 'title2dfg',
+    category: ['category2', 'category3'],
+    favorite: false,
+    description: 'description2',
+    solution: 'solution2',
+    insights: 'insights2',
+    severity: 2,
+    frequency: 2,
+    status: 'active',
+  },
+  {
+    timestamp: '2021-09-03',
+    title: 'title3',
+    category: ['category3', 'category4'],
+    favorite: true,
+    description: 'description3',
+    solution: 'solution3',
+    insights: 'insights3',
+    severity: 3,
+    frequency: 3,
+    status: 'active',
+  },
+];
+
+export const logListAtom = atom<FormFields[]>(mockDatalist);
--- src/pages/ActiveListPage.tsx
+++ src/pages/ActiveListPage.tsx
@@ -1,45 +1,9 @@
-import { FormFields } from '../types';
 import LogItem from '../components/LogItem';
-const mockDatalist: FormFields[] = [
-  {
-    timestamp: '2021-09-01',
-    title: 'title1',
-    category: ['category1', 'category2'],
-    favorite: true,
-    description: 'description1lsdkfjlsdkfjlsdkfjalsdkfjalsdfkjaldfkjaldkfjl',
-    solution: 'solution1sdf.kjsdlfkjsdlfkjsldfkja',
-    insights: 'insights1dfglkjfdlgkdjfgl',
-    severity: 1,
-    frequency: 1,
-    status: 'active',
-  },
-  {
-    timestamp: '2021-09-02',
-    title: 'title2',
-    category: ['category2', 'category3'],
-    favorite: false,
-    description: 'description2',
-    solution: 'solution2',
-    insights: 'insights2',
-    severity: 2,
-    frequency: 2,
-    status: 'active',
-  },
-  {
-    timestamp: '2021-09-03',
-    title: 'title3',
-    category: ['category3', 'category4'],
-    favorite: true,
-    description: 'description3',
-    solution: 'solution3',
-    insights: 'insights3',
-    severity: 3,
-    frequency: 3,
-    status: 'active',
-  },
-];
+import { useAtom } from 'jotai';
+import { logListAtom } from '../atom';
 
 const ActiveListPage = () => {
+  const [logList, setLogList] = useAtom(logListAtom);
   return (
     <div className="flex flex-col items-center gap-4 p-4 pt-6 w-full">
       <div className="w-80 border-2 bg-slate-300">로그 추가</div>
@@ -49,7 +13,7 @@ const ActiveListPage = () => {
         <div className="bg-slate-200">옵션</div>
       </div>
       <div className="w-full h-full">
-        {mockDatalist.map((data, index) => (
+        {logList.map((data, index) => (
           <LogItem key={index} {...data} />
         ))}
       </div>
