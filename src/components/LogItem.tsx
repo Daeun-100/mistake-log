2004262d9b1714c08ba1201d50ee5226070c97f7
Ko Daeun <drechsu@gmail.com>
2024-12-27 00:31:59 +0900

    feat(LogItem) : 즐겨찾기 클릭시 상태 변화

--- src/components/LogItem.tsx
+++ src/components/LogItem.tsx
@@ -1,7 +1,7 @@
 import { LogItemFields } from '../types';
 import FavoriteOn from '../assets/favorite.svg?react';
 import FavoriteNo from '../assets/favorite2.svg?react';
-import { selectedIdAtom } from '../atom';
+import { selectedIdAtom, logListAtom } from '../atom';
 import { useAtom } from 'jotai';
 import { useNavigate } from 'react-router-dom';
 
@@ -12,25 +12,40 @@ const LogItem: React.FC<LogItemFields> = ({
   category,
   favorite,
 }) => {
+  const [logList, setLogList] = useAtom(logListAtom);
   const [selectedId, setSelectedId] = useAtom(selectedIdAtom);
   const navigate = useNavigate();
 
-  const handleClick = () => {
+  const handleItemClick = () => {
     setSelectedId(id);
     navigate('/detail');
+  };
+
+  const handleFavoriteClick = (event: React.MouseEvent) => {
+    event.stopPropagation();
+    const newLogList = logList.map((log) => {
+      if (log.id === id) {
+        return { ...log, favorite: !log.favorite };
+      }
+      return log;
+    });
+    setLogList(newLogList);
   };
 
   return (
     <div
       className="flex justify-between items-center border-b-2 border-gray-400"
-      onClick={handleClick}
+      onClick={handleItemClick}
     >
-      <div className="flex flex-col pl-4">
+      <div className="flex flex-col pl-4  onClick={handleItemClick}">
         <div>{timestamp.toLocaleString()}</div>
         <div className="text-lg font-semibold ">{title}</div>
         <div>{category.join()}</div>
       </div>
-      <div className="flex items-center justify-center pr-4">
+      <div
+        className="flex items-center justify-center pr-4 p-3 "
+        onClick={handleFavoriteClick}
+      >
         {favorite ? (
           <FavoriteOn width={22} height={22} />
         ) : (
