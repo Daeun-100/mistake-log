6657d04a627ce8b8e080819cc350021e233f0e42
Ko Daeun <drechsu@gmail.com>
2024-12-27 00:02:16 +0900

    feat(LogDetailPage) : 삭제 기능 구현

--- src/pages/LogDetailPage.tsx
+++ src/pages/LogDetailPage.tsx
@@ -2,13 +2,14 @@ import LogDetail from '../components/LogDetail';
 import { useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import MistakeForm from '../components/mistakeForm/MistakeForm';
-import { selectedIdAtom } from '../atom';
+import { selectedIdAtom, logListAtom } from '../atom';
 import { useAtom } from 'jotai';
 
 const LogDetailPage: React.FC = () => {
   const navigate = useNavigate();
   const [isEdditing, setIsEdditing] = useState(false);
   const [selectedId, setSelectedId] = useAtom(selectedIdAtom);
+  const [logList, setLogList] = useAtom(logListAtom);
 
   const handleClickBack = () => {
     if (isEdditing) {
@@ -28,11 +29,17 @@ const LogDetailPage: React.FC = () => {
     setIsEdditing(false);
   };
 
+  const handleClickDelete = () => {
+    setLogList(logList.filter((log) => log.id !== selectedId));
+    navigate('/');
+  };
+
   return (
     <div>
       <div>LogDetailPage</div>
       <div onClick={handleClickBack}>뒤로가기</div>
       <div onClick={handleClickEdit}>수정하기</div>
+      <div onClick={handleClickDelete}>삭제하기</div>
       {isEdditing ? (
         <MistakeForm onClickSubmit={handleClickSubmit} />
       ) : (
