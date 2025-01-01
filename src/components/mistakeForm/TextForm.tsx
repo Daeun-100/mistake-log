6db904a6c096e2c1a6fa06c80a503ee7846fa339
Ko Daeun <drechsu@gmail.com>
2025-01-01 14:13:56 +0900

    fix : FormFields 오타수정

--- src/components/mistakeForm/TextForm.tsx
+++ src/components/mistakeForm/TextForm.tsx
@@ -1,11 +1,11 @@
 import { Path, UseFormRegister } from 'react-hook-form';
-import { TextFormFields, FormFileds } from '../../types';
+import { TextFormFields, FormFields } from '../../types';
 //내용,해결방법,해결책, 느낀점
 //key :  description solution insights
 
 type InputProps = {
   label: Path<TextFormFields>;
-  register: UseFormRegister<FormFileds>;
+  register: UseFormRegister<FormFields>;
   required: string;
 };
 
